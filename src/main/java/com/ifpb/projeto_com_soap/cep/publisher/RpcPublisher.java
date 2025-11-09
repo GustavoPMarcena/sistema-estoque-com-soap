702c5cf5853e054452d336d76da82c107fd7f27d
GustavoPMarcena <gustavopmarcena@outlook.com>
2025-11-09 16:23:06 -0300

    feat: add rpc impl

---
+++ src/main/java/com/ifpb/projeto_com_soap/cep/publisher/RpcPublisher.java
@@ -0,0 +1,12 @@
+package com.ifpb.projeto_com_soap.cep.publisher;
+
+import com.ifpb.projeto_com_soap.cep.service.CepServiceImpl;
+import jakarta.xml.ws.Endpoint;
+
+public class RpcPublisher {
+    public static void main(String[] args) {
+        CepServiceImpl service = new CepServiceImpl();
+        Endpoint.publish("http://localhost:8080/cep-service", service);
+        System.out.println("RPC SOAP service running at http://localhost:8080/cep-service");
+    }
+}
---
+++ src/main/java/com/ifpb/projeto_com_soap/cep/service/CepService.java
@@ -0,0 +1,20 @@
+package com.ifpb.projeto_com_soap.cep.service;
+
+import com.ifpb.projeto_com_soap.cep.Endereco;
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+import jakarta.jws.WebResult;
+import jakarta.jws.WebService;
+import jakarta.jws.soap.SOAPBinding;
+
+@WebService(
+        targetNamespace = "http://ifpb.com/projeto_com_soap/cep",
+        name = "CepService"
+)
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public interface CepService {
+
+    @WebMethod
+    @WebResult(name = "endereco")
+    Endereco getCep(@WebParam(name = "cep") String cep);
+}
